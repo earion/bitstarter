c8c6bdf570e1bff7140718d3d9d6f457544d1794
earion <m.batok@op.pl>
2013-07-21 21:14:32 +0000

    added read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var indexContent = fs.readFileSync("index.html");
+  response.send(indexContent);
 });
 
 var port = process.env.PORT || 5000;
